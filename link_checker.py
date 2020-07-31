612288eb306aee4cba50522a74bee5d7fe8c0367
endlesstrax <ricky@whitelionmedia.com>
2020-07-30 23:20:37 -0400

    Updated to use click. Parses and grabs the results dict() and displays prints results in the terminal.

--- link_checker.py
+++ link_checker.py
@@ -1,6 +1,21 @@
-from checker.url import check_response_code
+import click
+
+from checker.parser import find_all_anchor_tags, build_dicionary_of_links
+
+
+@click.command()
+@click.argument('url')
+def main(url: str):
+    """TODO: """
+    page_links = find_all_anchor_tags(url)
+    results = build_dicionary_of_links(url, page_links)
+
+    for k, v in results.items():
+        if v == 200:
+            click.echo(click.style(f"{v} - {k}", fg="green"))
+        else:
+            click.echo(click.style(f"{v} - {k}", fg="yellow"))
 
 
 if __name__ == "__main__":
-    test = check_response_code('https://google.com')
-    print(test)
+    main()
