b2b227f21ed82b7fd4fecad3d85a545c8e49ca38
endlesstrax <ricky@whitelionmedia.com>
2020-08-04 15:29:04 -0400

    Switched click for Typer

--- link_checker.py
+++ link_checker.py
@@ -1,21 +1,25 @@
-import click
+import typer
 
 from checker.parser import find_all_anchor_tags, build_dicionary_of_links
 
+app = typer.Typer()
 
-@click.command()
-@click.argument('url')
+@app.command()
 def main(url: str):
     """TODO: """
+
+    if not url.startswith('http'):
+        raise ValueError('Please provide a URL with a valid HTTP scheme')
+
     page_links = find_all_anchor_tags(url)
     results = build_dicionary_of_links(url, page_links)
 
     for k, v in results.items():
         if v == 200:
-            click.echo(click.style(f"{v} - {k}", fg="green"))
+            typer.echo(typer.style(f"{v} - {k}", fg="green"))
         else:
-            click.echo(click.style(f"{v} - {k}", fg="yellow"))
+            typer.echo(typer.style(f"{v} - {k}", fg="yellow"))
 
 
 if __name__ == "__main__":
-    main()
+    app()
