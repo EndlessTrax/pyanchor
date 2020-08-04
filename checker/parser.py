e75041e84517f271614eeca42b29cfae13256b03
endlesstrax <ricky@whitelionmedia.com>
2020-08-04 15:28:41 -0400

    Removed unused try/except

--- checker/parser.py
+++ checker/parser.py
@@ -1,7 +1,6 @@
 import requests
 from click.utils import echo
 from bs4 import BeautifulSoup
-from requests.models import MissingSchema
 
 from .url import check_response_code, check_link_for_http_scheme
 
@@ -9,16 +8,13 @@ from .url import check_response_code, check_link_for_http_scheme
 def find_all_anchor_tags(url: str):
     """TODO: Scrapes the given url/link for all anchor tags and returns a <class 'bs4.element.ResultSet'>"""
 
-    try:  # Ensure the url link is formatted correctly and involves a http scheme.
-        r = requests.get(url)
-        if r.status_code == 200:
-            soup = BeautifulSoup(r.content, "html.parser")
-            return soup.find_all("a")
+    r = requests.get(url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "html.parser")
+        return soup.find_all("a")
 
-        else:  # If not a 200 response, then an Exception is raised
-            raise Exception(f"The requested page is unavailable -> {url}")
-    except MissingSchema as e:  # Built in Exception from the requests library.
-        echo(e)
+    else:  # If not a 200 response, then an Exception is raised
+        raise Exception(f"The requested page is unavailable -> {url}") #TODO: Find better exception type/create custom Exception
 
 
 def build_dicionary_of_links(base_url, atags) -> dict:
