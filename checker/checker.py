0249d571d3e82fcc5b4ed279c5296d5660498bdc
endlesstrax <ricky@whitelionmedia.com>
2020-08-06 16:51:28 -0400

    Split build dict function into two.

--- checker/checker.py
+++ checker/checker.py
@@ -7,7 +7,8 @@ class LinkResults():
 
     def __init__(self, url: str):
         self.base_url = url
-        self.results = self.build_results_dictionary(url)
+        self.all_atags = self.find_all_atags()
+        self.results = self.build_results_dictionary()
 
 
     def __str__(self) -> str:
@@ -17,6 +18,7 @@ class LinkResults():
 
     def check_link_for_http_scheme(self, href: str) -> str:
         """TODO: """
+
         if href.startswith(self.base_url):
             return href
         elif href.startswith("/"):
@@ -26,15 +28,20 @@ class LinkResults():
             return href  # TODO: Deal with ./ or ../ relative links. And #links
 
 
-    def build_results_dictionary(self, url: str) -> dict:
+    def find_all_atags(self):
         """TODO:"""
-        r = requests.get(url)
+
+        r = requests.get(self.base_url)
         if r.status_code == 200:
             soup = BeautifulSoup(r.content, "html.parser")
-            self.all_atags = soup.find_all("a")
+            return soup.find_all("a")
 
         else:  # If not a 200 response, then an Exception is raised
-            raise Exception(f"The requested page is unavailable -> {url}") #TODO: Find better exception type/create custom Exception
+            raise Exception(f"The requested page is unavailable -> {self.base_url}") #TODO: Find better exception type/create custom Exception
+
+
+    def build_results_dictionary(self) -> dict:
+        """TODO:"""
 
         results = dict()
         for tag in self.all_atags:
