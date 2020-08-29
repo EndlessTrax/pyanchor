3a637bc41d7b1cc8b221150a1090767aa62d1816
endlesstrax <ricky@whitelionmedia.com>
2020-08-29 10:16:35 -0400

    Moved the printing of results to own function. Fixed --sitemap results.

--- link_checker.py
+++ link_checker.py
@@ -1,23 +1,52 @@
+import pprint
 import typer
+import requests
+from bs4 import BeautifulSoup
 
 from checker.checker import LinkResults
 
 app = typer.Typer()
 
+def print_result(link: dict):
+    for k, v in link.items():
+        if v == 200:
+            typer.echo(typer.style(f"[ {v} ] - {k}", fg="green"))
+        else:
+            typer.echo(typer.style(f"[ {v} ] - {k}", fg="yellow"))
+
+
 @app.command()
-def main(url: str):
+def main(url: str, 
+        sitemap: bool = typer.Option(False, '--sitemap', help="Use if URL is a sitemap.xml link"),
+    ):
     """TODO: """
 
     if not url.startswith('http'):
         raise ValueError('Please provide a URL with a valid HTTP scheme')
 
-    results = LinkResults(url).results
+    if sitemap is True:
+        set_of_urls = set()
+        results = dict()
 
-    for k, v in results.items():
-        if v == 200:
-            typer.echo(typer.style(f"{v} - {k}", fg="green"))
-        else:
-            typer.echo(typer.style(f"{v} - {k}", fg="yellow"))
+        r = requests.get(url)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, "lxml")
+            sitemap_links = soup.find_all("loc")
+
+            for sitemap_link in sitemap_links:
+                set_of_urls.add(sitemap_link.text)
+
+        for _url in set_of_urls:
+            link_result = LinkResults(_url).results
+            
+            for k, v in link_result.items():
+                results[k] = v
+
+        print_result(results)
+
+    else:
+        results = LinkResults(url).results
+        print_result(results)
 
 
 if __name__ == "__main__":
