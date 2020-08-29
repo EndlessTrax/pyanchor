06bee9782869e1a6aaf8ce32f991e254f8bdfa35
endlesstrax <ricky@whitelionmedia.com>
2020-08-29 10:15:17 -0400

    Changes to checker.py

--- checker/checker.py
+++ checker/checker.py
@@ -7,7 +7,7 @@ class LinkResults():
 
     def __init__(self, url: str):
         self.base_url = url
-        self.all_atags = self.find_all_atags()
+        self.all_atags = self.find_all_atags(self.base_url)
         self.results = self.build_results_dictionary()
 
 
@@ -25,19 +25,19 @@ class LinkResults():
             href = self.base_url + href
             return href
         else:
-            return href  # TODO: Deal with ./ or ../ relative links. And #links
+            return None  # TODO: Deal with ./ or ../ relative links.
 
 
-    def find_all_atags(self):
+    def find_all_atags(self, url: str): # Returns -> bs4.element.ResultSet
         """TODO:"""
 
-        r = requests.get(self.base_url)
+        r = requests.get(url)
         if r.status_code == 200:
             soup = BeautifulSoup(r.content, "html.parser")
             return soup.find_all("a")
 
         else:  # If not a 200 response, then an Exception is raised
-            raise Exception(f"The requested page is unavailable -> {self.base_url}") #TODO: Find better exception type/create custom Exception
+            raise Exception(f"The requested page is unavailable -> {url}") #TODO: Find better exception type/create custom Exception
 
 
     def build_results_dictionary(self) -> dict:
@@ -47,6 +47,15 @@ class LinkResults():
         for tag in self.all_atags:
             href = tag.get("href")
             parsed_url = self.check_link_for_http_scheme(href)
-            results[parsed_url] = requests.get(parsed_url).status_code
+
+            if parsed_url is not None:
+                results[parsed_url] = requests.get(parsed_url).status_code
+            else: 
+                pass
             
         return results
+
+    # @property
+    # def check_anchor_tags_attributes(self, tags):
+    #     """TODO:"""
+    #     return 
