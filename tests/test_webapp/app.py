4e516a971756ebb042e5dea0b6117190788bd83f
endlesstrax <ricky@whitelionmedia.com>
2020-09-03 12:19:56 -0400

    Added a flask app for testing the pyanchor package

---
+++ tests/test_webapp/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("success.html", name="About")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("success.html", name="Contact")
+
+
+@app.route("/login")
+def login():
+    return render_template("error.html", status_code=401), 401
+
+
+@app.route("/500")
+def five_hundred():
+    return render_template("error.html", status_code=500), 500
+
+
+if __name__ == "__main__":
+    app.run()
