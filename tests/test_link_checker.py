90882240e5cb1e4c876e5166dc74c897d10d9d4a
endlesstrax <ricky@whitelionmedia.com>
2020-09-03 13:45:27 -0400

    Added tests + painted black

--- tests/test_link_checker.py
+++ tests/test_link_checker.py
@@ -8,6 +8,12 @@ from pyanchor.link_checker import LinkResults
 runner = CliRunner()
 
 
+@pytest.fixture
+def example_object():
+    """TODO:"""
+    return LinkResults("http://127.0.0.1:5000")
+
+
 def test_exception_on_invalid_url_http_scheme():
     """TODO:"""
 
@@ -22,9 +28,10 @@ def test_successful_result_prints():
     assert "[ 200 ] - http://127.0.0.1:5000/about" in results.output
 
 
-@pytest.fixture
-def example_object():
-    return LinkResults("http://127.0.0.1:5000")
+def test_failing_result_prints():
+    """TODO:"""
+    results = runner.invoke(app, ["http://127.0.0.1:5000"])
+    assert "[ 500 ] - http://127.0.0.1:5000/500" in results.output
 
 
 def test_link_result_class_str(example_object):
@@ -36,3 +43,7 @@ def test_results_is_dict(example_object):
     """TODO:"""
     assert isinstance(example_object.results, dict)
 
+
+def test_raises_exception_on_unavailable_base_url():
+    with pytest.raises(Exception):
+        LinkResults("http://127.0.0.1:5000/notareallink")
