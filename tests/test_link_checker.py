13f515f7c1e20d0d9d63521085c2eda1e10b645b
Ricky White <ricky@whitelionmedia.com>
2020-10-16 13:47:16 -0400

    Added tests for relative links.

--- tests/test_link_checker.py
+++ tests/test_link_checker.py
@@ -14,3 +14,7 @@ class TestLinkChecker:
 
     def test_link_result_class_str(self, example_object):
         assert str(example_object) == "All links for http://127.0.0.1:5000/"
+
+    def test_successful_parse_relative_links(self, example_object):
+        assert "http://127.0.0.1:5000/rel" in example_object.results[200]
+        assert "http://127.0.0.1:5000/rel2" in example_object.results[200]
