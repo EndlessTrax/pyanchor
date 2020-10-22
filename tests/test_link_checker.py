591782d0028ba951b814028563a75802577ced24
Ricky White <ricky@whitelionmedia.com>
2020-10-22 11:16:50 -0400

    Added tests for LinkAnalysis and fixed broken tests after PRs.

--- tests/test_link_checker.py
+++ tests/test_link_checker.py
@@ -19,6 +19,10 @@ class TestLinkResults:
     def test_link_result_class_str(self, example_LinkResults_object):
         assert str(example_LinkResults_object) == "All links for http://127.0.0.1:5000/"
 
+    def test_successful_parse_relative_links(self, example_LinkResults_object):
+        assert "http://127.0.0.1:5000/rel" in example_LinkResults_object.results[200]
+        assert "http://127.0.0.1:5000/rel2" in example_LinkResults_object.results[200]
+
 
 class TestLinkAnalysis:
     def test_obsolete_attrs_returns_dict(self, example_LinkAnalysis_object):
@@ -27,11 +31,7 @@ class TestLinkAnalysis:
     def test_obsolete_attrs_on_tag(self, example_LinkAnalysis_object):
         attr_list = example_LinkAnalysis_object.obsolete_attrs
         assert attr_list["/about/link-1"][0] == "charset"
-        assert attr_list["/about/link-1"][1] == "name"
-        
-    def test_link_result_class_str(self, example_object):
-        assert str(example_object) == "All links for http://127.0.0.1:5000/"
+        assert attr_list["/about/link-2"][0] == "name"
 
-    def test_successful_parse_relative_links(self, example_object):
-        assert "http://127.0.0.1:5000/rel" in example_object.results[200]
-        assert "http://127.0.0.1:5000/rel2" in example_object.results[200]
+    def test_unsafe_attrs_returns_dict(self, example_LinkAnalysis_object):
+        assert isinstance(example_LinkAnalysis_object.unsafe_attrs, dict)
