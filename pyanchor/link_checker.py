43c422b804cbd613232afd353a4b8e8f4d36927b
Ricky White <ricky@whitelionmedia.com>
2020-10-22 12:10:42 -0400

    Updated docstrings

--- pyanchor/link_checker.py
+++ pyanchor/link_checker.py
@@ -1,6 +1,6 @@
 """The main parser for finding and checking links
 
-The LinkResults class expects a URL as it's only argument. It will return a 
+The LinkResults class expects a URL as its only argument. It will return a 
 dictionary of all links on the given html page which is stores in the object.results 
 attribute. The returning dictionary consists of the link (dict value) and the http 
 response (dict key).
@@ -8,14 +8,26 @@ response (dict key).
     Typical usage example:
 
     results = LinkResults(url).results
+
+The LinkAnalysis class expects a URL as its only argument, also. It returns a 
+dictionary of all links that have the `target` attribute, and then analyzes to 
+see if they are safe links (i.e. have a `rel` attribute, too). Links are also 
+checked for obsolote attributes.
+
+    Typical usage example:
+
+    obsolete = LinkAnalysis(url).obsolete_attrs
+    or
+    unsafe = LinkAnalysis(url).unsafe_attrs
 """
 
 import requests
 from bs4 import BeautifulSoup
 import re
+from typing import Type
+
 
 class AllTags:
-
     def __init__(self, url: str):
         if url.endswith("/"):
             self.base_url = url
@@ -27,7 +39,7 @@ class AllTags:
     def __str__(self) -> str:
         return f"All links for {self.base_url}"
 
-    def find_all_atags(self, url: str):
+    def find_all_atags(self, url: str) -> bs4.element.ResultSet:
         """Find all anchor tags on a given URL.
         
         Args:
@@ -47,7 +59,6 @@ class LinkResults(AllTags):
     def __init__(self, url: str):
         super().__init__(url)
         self.results = self.build_results_dictionary()
-
 
     def check_link_for_http_scheme(self, href: str) -> str:
         """Checks a link for http scheme.
@@ -76,17 +87,16 @@ class LinkResults(AllTags):
             return self.base_url + re.sub("../", "", href)
 
         else:
-            return None # Catches any links starting with #
-
+            return None  # Catches any links starting with hash(#)
 
     def build_results_dictionary(self) -> dict:
         """Build the final results dictionary.
         
-        Once all_tags has been populated, the final results dictionary is build by 
-        testing each link. 
+        Once all_tags has been populated in the AllTags base class, the final 
+        results dictionary is build by testing each link.
 
         Returns:
-            A dictionary with the key = the HTTP response, 
+            A dictionary with the key = the HTTP response,
             and the value = a List of URLs that achieved that response code.
         """
 
@@ -110,29 +120,26 @@ class LinkResults(AllTags):
 
 
 class LinkAnalysis(AllTags):
-    """
-    Check the anchor tag for:
-        taget attr of any kind:
-            check to see if rel="noreferrer noopener"
-        missing Title attr
-        Check for obsolete attrs
-            charset
-            coords
-            name
-            rev 
-            shape
-
-    Print output to user
-    """
-
     def __init__(self, url: str):
         super().__init__(url)
         self.obsolete_attrs = self.obsolete_attributes(self.all_atags)
         self.unsafe_attrs = self.unsafe_attributes(self.all_atags)
 
-    def obsolete_attributes(self, links: str) -> dict:
-        OBSOLETE_ATTRS = ('charset', 'coords', 'name', 'rev', 'shape')
+    def obsolete_attributes(self, links: Type[bs4.element.ResultSet]) -> dict:
+        """ Check links for the presence of obsolete attributes
         
+        Args:
+            Takes a bs4 ResultSet which is returned from AllTags in self.all_atags
+
+        Returns:
+            Returns a dictionary with the key being the `href` of the link, 
+            and the value being a `List` of obsolete attributes that link has. 
+            Any link with 0 obsolete attributes is ingnored.
+
+        """
+
+        OBSOLETE_ATTRS = ("charset", "coords", "name", "rev", "shape")
+
         return_dict = dict()
         for link in links:
             obs_link_attrs = []
@@ -144,16 +151,32 @@ class LinkAnalysis(AllTags):
                 href = link["href"]
                 if href in return_dict:
                     return_dict[href].append(*obs_link_attrs)
-                else: 
+                else:
                     return_dict[href] = obs_link_attrs
-        
+
         return return_dict
 
-    def unsafe_attributes(self, links: str) -> dict:
+    def unsafe_attributes(self, links: Type[bs4.element.ResultSet]) -> dict:
+        """ Checks to see if links are unsafe.
+
+        All links with the `target` attribute are checked for the presence/absence
+        of a `rel="noopener"`. This should be present on all anchor tags with the
+        `target` attribute to ensure the linked site does not have access to 
+        window.opener property. 
+        
+        For more details: https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noopener 
+
+        Args:
+            Takes a bs4 ResultSet which is returned from AllTags in self.all_atags
+
+        Returns:
+            Returns a dictionary with the key being the full anchor tag checked, 
+            and the value being a `Bool`. False = safe link, True = unsafe.
+        """
         return_dict = dict()
         for link in links:
-            if link.get('target'):
-                if link.get('rel') and 'noopener' in link.get('rel'):
+            if link.get("target"):
+                if link.get("rel") and "noopener" in link.get("rel"):
                     return_dict[link] = False
                 else:
                     return_dict[link] = True
