10bcdb906df9019a5525a0f6c4b1097d1cb5e559
Ricky White <ricky@whitelionmedia.com>
2023-05-25 19:52:17 -0400

    Fix logic

--- pyanchor/filters.py
+++ pyanchor/filters.py
@@ -1,11 +1,9 @@
-from http.client import responses
-
 from pyanchor.types import AnchorTag
 
 
 def filter_for_unsafe_links(anchor_tags: list[AnchorTag]) -> None:
     for atag in anchor_tags:
-        if not atag.check_is_unsafe():
+        if atag.check_is_unsafe():
             anchor_tags.remove(atag)
 
 
@@ -17,5 +15,5 @@ def filter_for_obsolete_attrs(anchor_tags: list[AnchorTag]) -> None:
 
 def filter_for_http_not_OK(anchor_tags: list[AnchorTag]) -> None:
     for atag in anchor_tags:
-        if atag.status_code and responses[atag.status_code] == "OK":
+        if atag.status_code == 200:
             anchor_tags.remove(atag)
