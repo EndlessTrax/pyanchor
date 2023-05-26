eec3d67282d8079d9fb02d2de6b39c60e470354c
Ricky White <ricky@whitelionmedia.com>
2023-05-25 20:04:29 -0400

    Add more tests

--- tests/test_pyanchor.py
+++ tests/test_pyanchor.py
@@ -6,6 +6,56 @@ from pyanchor.filters import (
 )
 
 
+class TestAnchorTag:
+    def test_str(self):
+        anchor = AnchorTag(
+            '<a href="/link-1">Link 1</a>',
+            {"href": "/link-1", "text": "Link 1"},
+            "http://example.com",
+            "/link-1",
+        )
+        assert str(anchor) == '<a href="/link-1">Link 1</a>'
+
+    def test_build_qualified_url(self):
+        anchor_tags = [
+            AnchorTag(
+                '<a href="/link-1">Link 1</a>',
+                {"href": "/link-1", "text": "Link 1"},
+                "http://example.com",
+                "/link-1",
+            ),
+            AnchorTag(
+                '<a href="http://example.com/link-2">Link 2</a>',
+                {"href": "http://example.com/link-2", "text": "Link 2"},
+                "http://example.com",
+                "http://example.com/link-2",
+            ),
+            AnchorTag(
+                '<a href="./link-3">Link 3</a>',
+                {"href": "./link-3", "text": "Link 3"},
+                "http://example.com",
+                "./link-3",
+            ),
+            AnchorTag(
+                '<a href="../link-4">Link 4</a>',
+                {"href": "../link-4", "text": "Link 4"},
+                "http://example.com",
+                "../link-4",
+            ),
+            AnchorTag(
+                '<a href="#">Link 5</a>',
+                {"href": "#", "text": "Link 5"},
+                "http://example.com",
+                "#",
+            ),        
+        ]
+        assert anchor_tags[0].qualified_url == "http://example.com/link-1"
+        assert anchor_tags[1].qualified_url == "http://example.com/link-2"
+        assert anchor_tags[2].qualified_url == "http://example.com/link-3"
+        assert anchor_tags[3].qualified_url == "http://example.com/link-4"
+        assert anchor_tags[4].qualified_url is None
+
+
 class TestFilters:
     def test_filter_for_unsafe_links(self):
         unsafe = [
@@ -30,7 +80,6 @@ class TestFilters:
         filter_for_unsafe_links(unsafe)
         assert len(unsafe) == 1
 
-
     def test_filter_for_obsolete_attrs(self):
         obsolete = [
             AnchorTag(
@@ -53,7 +102,6 @@ class TestFilters:
         ]
         filter_for_obsolete_attrs(obsolete)
         assert len(obsolete) == 1
-
 
     def test_filter_for_http_not_OK(self):
         not_okay = [
