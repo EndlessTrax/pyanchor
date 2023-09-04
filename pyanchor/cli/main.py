8a79d06980fe67ca4fc441a07ed1101080341509
Ricky White <ricky@rickywhite.net>
2023-09-04 15:00:12 -0400

    Cleanup main.py

--- pyanchor/cli/main.py
+++ pyanchor/cli/main.py
@@ -8,16 +8,3 @@ app.add_typer(check_app, name="check")
 
 if __name__ == "__main__":
     app()
-
-"""
-anchors check url <url> [options]
-
-anchors check sitemap <file> [options]
-
-Options:
-  -v, --version				output the version number
-  -o, --output <file>		output results to a file
-  -a, --all					Show all results, includes HTTP OK
-  -u, --unsafe				Show unsafe and unsupported attributes
-  -h, --help				output usage information
-"""
