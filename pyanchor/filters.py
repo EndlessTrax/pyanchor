2a3cb84c2d764ec3f50ee5025536149ea765c3c5
Ricky White <ricky@rickywhite.net>
2023-09-04 15:01:21 -0400

    Add docstrings

--- pyanchor/filters.py
+++ pyanchor/filters.py
@@ -2,17 +2,17 @@ from pyanchor.types import AnchorTag
 
 
 def filter_for_unsafe_links(anchor_tags: list[AnchorTag]) -> list[AnchorTag]:
-    """#TODO: Add docstring"""
+    """Filter a list of AnchorTags for unsafe links."""
     return [atag for atag in anchor_tags if atag.check_is_unsafe()]
 
 
 def filter_for_obsolete_attrs(anchor_tags: list[AnchorTag]) -> list[AnchorTag]:
-    """#TODO: Add docstring"""
+    """Filter a list of AnchorTags for obsolete attributes."""
     return [atag for atag in anchor_tags if len(atag.check_for_obsolete_attrs()) > 0]
 
 
 def filter_for_http_not_OK(anchor_tags: list[AnchorTag]) -> list[AnchorTag]:
-    """#TODO: Add docstring"""
+    """Filter a list of AnchorTags for HTTP status codes that are not 200."""
     return [
         atag
         for atag in anchor_tags
